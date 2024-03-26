514e884613090e2e2412f1927c0675774af05eff
VaishnaviSreejith <vaishnaadevii@gmail.com>
2024-03-26 12:15:56 +0000

    updated compiler version

--- src/test/java/org/runner/Testrunner.java
+++ src/test/java/org/runner/Testrunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/Featurefiles/", glue = {
+@CucumberOptions(features = "src/test/resources/Featurefiles/", plugin= {"json:cucumber.json"},glue = {
 		"org.stepdefinition", "org.hooks" }, dryRun = false)
 
 public class Testrunner {
