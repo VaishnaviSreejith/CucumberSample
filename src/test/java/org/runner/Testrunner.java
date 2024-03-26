c44a457e6cf37cceca7a00019b87539cc7e3d523
VaishnaviSreejith <vaishnaadevii@gmail.com>
2024-03-26 11:06:20 +0000

    updated compiler version

--- src/test/java/org/runner/Testrunner.java
+++ src/test/java/org/runner/Testrunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\vaish\\eclipse-workspace\\CucumberSample\\src\\test\\resources\\Featurefiles\\", glue = {
+@CucumberOptions(features = "src/test/resources/Featurefiles/", glue = {
 		"org.stepdefinition", "org.hooks" }, dryRun = false)
 
 public class Testrunner {
