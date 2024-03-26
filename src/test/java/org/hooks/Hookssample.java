89510b0832dd493b2de00920466935b7bef6b100
VaishnaviSreejith <vaishnaadevii@gmail.com>
2024-03-26 11:33:22 +0000

    updated compiler version

--- src/test/java/org/hooks/Hookssample.java
+++ src/test/java/org/hooks/Hookssample.java
@@ -1,16 +1,18 @@
 package org.hooks;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.stepdefinition.BaseTest;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Hookssample extends BaseTest {
 
 	@Before
 	public void before() {
-		//WebDriverManager.chromedriver().setup();
+		WebDriverManager.chromedriver().browserVersion("123.0.6312.59");
 		driver=new ChromeDriver();
 //		String title = driver.getTitle();
 //		System.out.println(title);
